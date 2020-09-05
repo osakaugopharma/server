6f558767aa3b224bfe31e3bbbfa5fbb08491cebe
Kanu Chukwunonso <franknonso114@gmail.com>
2020-09-05 02:11:12 +0000

    remove unused dependencies

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var createError = require('http-errors');
-var cors = require('cors');
 var express = require('express');
 const formidableMiddleware = require('express-formidable');
 var path = require('path');
@@ -15,14 +14,11 @@ var flash = require('connect-flash');
 var validator = require('express-validator');
 var MongoStore = require('connect-mongo')(session);
 
-
 var indexRouter = require('./routes/index');
 var userRoutes = require('./routes/user');
 var adminRouter = require('./routes/admin.router');
 var app = express();
-app.use(cors());
 
-// mongoose.connect('mongodb://localhost:27017/shop', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect('mongodb+srv://oup_client:e02pq1vJD4gKBVMH@cluster0.jtray.mongodb.net/shop?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
